2fef19078cee2fdc97164cef39cf8e84bb9fd823
Nacho Ferrari <ignacio.ferrari@ing.austral.edu.ar>
2023-01-27 10:01:54 -0300

    feat: :art: moved error validation code outside of methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,7 @@ class UsersController < ApplicationController
             token = encode_token(@user.id)
             render json: { user: @user, jwt: token }, status: :created
         else
-            if @user.errors.any?
-                render json: @user.errors, status: :unprocessable_entity
-            else
-                render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
-            end
+            validate_errors(@user)
         end
     end
 
@@ -25,17 +21,19 @@ class UsersController < ApplicationController
             token = encode_token(@user.id)
             render json: { user: @user, jwt: token }, status: :ok
         else
-            if @user.errors.any?
-                render json: @user.errors, status: :unprocessable_entity
-            else
-                render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
-            end
+            validate_errors(@user)
         end
     end
 
     private 
-    
-    def user_params
-        params.require(:user).permit(:username, :password)
-    end
+        def user_params
+            params.require(:user).permit(:username, :password)
+        end
+        def validate_errors(user)
+            if user.errors.any?
+                render json: user.errors, status: :unprocessable_entity
+            else
+                render json: { error: 'Invalid username or password' }, status: :unprocessable_entity
+            end
+        end
 end
